e83d0bd909ae45413e8df0a788ff2970f55219a7
anapaulavaldetario <apvaldetario@yahoo.com.br>
2023-01-11 00:04:38 +0000

    alterações

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const buttonFirstSemester=document.querySelector(".first-semester>button#first-semester")
+const buttonSecondSemester=document.querySelector(".second-semester>button#second-semester")
+
+function opentable(){
+console.log("Hello Alef")
+}
+
+buttonFirstSemester.addEventListener("click",opentable)
+buttonSecondSemester.addEventListener("click",opentable)
+
